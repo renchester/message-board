ff3880f3f077501937109422a31795c654c240ae
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-27 19:10:17 +0800

    Add message api route handler

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,6 +1,66 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { body, validationResult } from 'express-validator';
+
+import Message from '../models/message';
+
 const router = express.Router();
 
-router.get('/messages', function (req, res) {});
+// Redirect to messages route
+router.get('/', (req, res) => res.redirect('/api/messages'));
+
+// GET all messages
+router.get(
+  '/messages',
+  asyncHandler(async (req, res, next) => {
+    const messages = await Message.find({}, { __v: 0 })
+      .sort({ date_created: -1 })
+      .exec();
+
+    //   res.render('index', { title: 'Mini Message Board', messages });
+    console.log(messages);
+    res.json({ messages: messages });
+  }),
+);
+
+// POST request fro creating new message
+router.post('/messages', [
+  // Validate and sanitize fields.
+  body('username', 'Username must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('username', 'Username must be below 50 characters.')
+    .trim()
+    .isLength({ max: 50 })
+    .escape(),
+  body('new_message', 'Message must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process requesdt after validation and sanitization.
+  asyncHandler(async (req, res) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a new Message object with escaped and trimmed data.
+    const message = new Message({
+      message: req.body.new_message,
+      username: req.body.username,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Redirect for now.
+      res.redirect('/api/messages');
+    } else {
+      // Data from form is valid.
+      // Save message.
+      await message.save();
+
+      res.redirect('/api/messages');
+    }
+  }),
+]);
 
 export default router;
