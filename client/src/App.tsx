bb96b2b93786a4eebc2d758d8811b3e9099362e4
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-29 16:43:46 +0800

    Add initial app content and states

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react';
+import MessageBoard from './components/MessageBoard';
+import MessageForm from './components/MessageForm';
+import Loading from './components/Loading';
+
 function App() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getMessages() {
+      const messageUrl = import.meta.env.VITE_API_URL;
+
+      setLoading(true);
+      const response = await fetch(`${messageUrl}/api/messages`, {
+        mode: 'cors',
+      });
+
+      const data = await response.json();
+      const { messages } = data;
+
+      if (messages) {
+        setMessages(messages);
+        setLoading(false);
+      }
+    }
+
+    getMessages();
+  }, []);
+
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div className="container">
+      <div className="board">
+        <h1 className="title">Message Board</h1>
+        {isLoading ? (
+          <Loading message="Loading messages..." />
+        ) : (
+          <MessageBoard messages={messages} />
+        )}
+        <MessageForm setMessages={setMessages} />
+      </div>
+    </div>
   );
 }
 
